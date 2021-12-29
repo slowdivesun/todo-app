96e183e18cad68ff90e03d94e3a216b74bce5f6b
Shivam <shivam.gup82@gmail.com>
2021-12-29 19:50:09 +0530

    added logic for posting a list

--- todo/src/routes/newList/NewList.jsx
+++ todo/src/routes/newList/NewList.jsx
@@ -1,24 +1,30 @@
+import axios from 'axios';
 import './newlist.css'
-// const { useState } = require("react");
+const { useState } = require("react");
 
 
 const NewList = () => {
 
-    // const [title, setTitle] = useState("");
+    const [title, setTitle] = useState("");
 
-    // const handleSubmit = async (e) => {
-    //     e.preventDefault();
-    //     const newList = {
-
-    //     }
-
-    // }
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const newList = {
+            listName: title
+        }
+        try {
+            const res = await axios.post("/list", newList);
+            console.log(res)
+        } catch (err) {
+            console.log(err)
+        }
+    }
 
     return (
         <div>
-            <form className="newlist-container">
+            <form className="newlist-container" onSubmit={handleSubmit}>
                 <label htmlFor="list">Enter Name</label>
-                <input type="text" id="list" />
+                <input type="text" id="list" onChange={(e) => setTitle(e.target.value)} />
                 <button type='submit'>Submit</button>
             </form>
         </div>
