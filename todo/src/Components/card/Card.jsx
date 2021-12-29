f4ececcedb6cbb71dfbabfe2fa0f456ca10151cb
Shivam <shivam.gup82@gmail.com>
2021-12-29 21:22:31 +0530

    fixed card UI

--- todo/src/Components/card/Card.jsx
+++ todo/src/Components/card/Card.jsx
@@ -7,7 +7,7 @@ export default function Card(props) {
     return (
         <div className='card-container' style={{ "backgroundColor": color }}>
             <Link to={`/list/${id}`}>
-                <div>{title}</div>
+                <div className='card-title'>{title}</div>
             </Link>
             <div className="card-items" > {items} items</div>
         </div >
